3c3f92645f0c49993768db07c0e566890e588329
Seyed Amir Mousavi <sayed1384mahdi@gmail.com>
2025-07-31 11:54:49 +0330

    add custom token generator for activation

--- bazargan/accounts/tokens.py
+++ bazargan/accounts/tokens.py
@@ -39,6 +39,6 @@ class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         email_field = user.get_email_field_name()
         email = getattr(user, email_field, "") or ""
-        return f"{user.pk}{user.password}{timestamp}{email}"
+        return f"{user.pk}{timestamp}{email}{user.is_user_verified}"
 
 activation_token_generator = AccountActivationTokenGenerator()
